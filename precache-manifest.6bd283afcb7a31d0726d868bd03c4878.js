df72342cd7ee93ca7e842bf549d13ee2e6fad540
@frontend.intern <joshih1998@gmail.com>
2020-09-07 21:01:57 +0530

    Updates

---
+++ precache-manifest.6bd283afcb7a31d0726d868bd03c4878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5401b27772ba64da792bfa04c49675d",
+    "url": "/APOD/index.html"
+  },
+  {
+    "revision": "997da3720b499e12c381",
+    "url": "/APOD/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "80d2c5417cbd560ec26f",
+    "url": "/APOD/static/css/main.a4fc4d9d.chunk.css"
+  },
+  {
+    "revision": "997da3720b499e12c381",
+    "url": "/APOD/static/js/2.0c4ed01f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/APOD/static/js/2.0c4ed01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d2c5417cbd560ec26f",
+    "url": "/APOD/static/js/main.515145dd.chunk.js"
+  },
+  {
+    "revision": "89b8457d72c5ce150021",
+    "url": "/APOD/static/js/runtime-main.fadcd104.js"
+  }
+]);
